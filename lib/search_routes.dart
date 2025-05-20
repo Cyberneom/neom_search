3e91b0ee10812ee6fb1dea450e9e279930f93fde
Emmanuel Montoya <yori.technologies@gmail.com>
2025-05-19 18:42:04 -0600

    Stable previous coupon, analytics wallet release and bookSearch

--- lib/search_routes.dart
+++ lib/search_routes.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:neom_commons/core/utils/constants/app_route_constants.dart';
 
 import 'search/ui/app_search_page.dart';
+import 'search/ui/itemlist/app_media_item_search_page.dart';
+import 'search/ui/itemlist/playlist_name_desc_page.dart';
 
 
 class SearchRoutes {
@@ -11,6 +13,21 @@ class SearchRoutes {
     GetPage(
       name: AppRouteConstants.search,
       page: () => const AppSearchPage(),
+    ),
+    GetPage(
+      name: AppRouteConstants.itemSearch,
+      page: () => const AppMediaItemSearchPage(),
+      transition: Transition.zoom,
+    ),
+    GetPage(
+      name: AppRouteConstants.playlistSearch,
+      page: () => const AppMediaItemSearchPage(),
+      transition: Transition.zoom,
+    ),
+    GetPage(
+        name: AppRouteConstants.playlistNameDesc,
+        page: () => const PlaylistNameDescPage(),
+        transition: Transition.leftToRight
     ),
   ];
 
